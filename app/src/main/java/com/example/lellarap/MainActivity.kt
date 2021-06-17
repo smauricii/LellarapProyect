810c10915ac4899384cfe9f0486a51372ad5ce86
smauricii <schamorro28@uan.edu.co>
2021-06-17 15:19:38 -0500

    rama main :)

--- app/src/main/java/com/example/lellarap/MainActivity.kt
+++ app/src/main/java/com/example/lellarap/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //esta es la rama main
+        //esta es la rama main :)
     }
 }
